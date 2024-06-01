0944948fd4149cfad519cfc049fa223cfd032a09
Tiago Azevedo <tiagogadenz@icloud.com>
2024-06-01 17:41:05 -0300

    update

--- Typescript - OOP/app.ts
+++ Typescript - OOP/app.ts
@@ -1,32 +1,15 @@
-interface Pessoa {
-    nome: string;
-    idade: number;
-    profissao?: string;
+
+
+class Person {
+    name: string;
+    age: number;
+    // Constructor method is a special method that is called when an instance of the class is created, forcing the new instance to have the properties.
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        console.log(`Hello, I am ${this.name}`);
+    }
 }
-
-function apresentar(pessoa: Pessoa) {
-    return `Olá, meu nome é ${pessoa.nome} e tenho ${pessoa.idade} anos.`;
-}
-
-const pessoa: Pessoa = {
-    nome: 'João',
-    idade: 30
-};
-
-const outraPessoa: Pessoa = {
-    nome: 'Maria',
-    idade: 25,
-    profissao: 'Desenvolvedora'
-};
-
-
-console.log(apresentar(pessoa));
-
-const pessoas: Pessoa[] = [
-    pessoa,
-    outraPessoa
-];
-
-console.log(pessoas);
-
-const numeros: number[] = [1, 2, 3, 4, 5];
