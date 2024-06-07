662c48248e88f7cbfa5e7e229fcc00c23c8c5644
Tiago Azevedo <tiagogadenz@icloud.com>
2024-06-07 19:49:11 -0300

    Using more examples of chakra UI

--- dio-banking-ts/src/App.tsx
+++ dio-banking-ts/src/App.tsx
@@ -1,27 +1,21 @@
 import { Layout } from "./components/Layout/Layout";
 import styled from "styled-components";
-import { ChakraProvider } from '@chakra-ui/react'
+import { 
+  ChakraProvider,
+  Input,
+  Center
+ } from '@chakra-ui/react'
 
 function App() {
-  const Box = styled.div`
-    background-color: orange;
-    padding: 10px;
-    border-radius: 5px;
-    margin-top: 10px;
-  `;
-
   return (
     <>
-      <Layout>
-        <Box>
+      <ChakraProvider>
+        <Center>
           <h1>Faça o login</h1>
-        </Box>
-        <label htmlFor="emailInput">Email</label>
-        <input id="emailInput" type="email" />
-        <label htmlFor="passwordInput">Senha</label>
-        <input id="passwordIput" type="password" />
-        <button>Entrar</button>
-      </Layout>
+          <Input placeholder="Email" />
+          <Input placeholder="Password" />
+        </Center>
+      </ChakraProvider>
     </>
   );
 }
